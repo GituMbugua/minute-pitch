1dfa65ef0d9346d94186dda40694cc5e9507c475
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-30 21:56:07 +0300

    add database shell

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
+from app.models import User, Role, Category, Pitch, Review
 
 # create app instance 
 app =  create_app('development')
@@ -10,8 +11,12 @@ manager.add_command('server', Server)
 def test():
     '''run utitest'''
     import unittest
-    tests=unittest.TestLoader().discover('test')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Role = Role, Category = Category, Pitch = Pitch, Review = Review)
 
 if __name__ == '__main__':
     manager.run()
