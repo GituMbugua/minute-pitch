9caea44874d342ff7d75ccf57b87778a1787508a
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-31 01:58:58 +0300

    add functionality to log out

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from . import auth
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
