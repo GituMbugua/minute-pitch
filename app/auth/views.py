b27acdb66665ac1828a5f2ac6afba24f01b7751a
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-31 01:57:30 +0300

    add login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,22 @@
 from . import auth
-from flask import render_template, redirect, url_for
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
 
-@auth.route('/login')
+@auth.route('/login', methods = ["GET", "POST"])
 def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or password')
+
+    title = "Login"
     return render_template('auth/login.html')
 
 @auth.route('/register', methods = ["GET", "POST"])
@@ -15,7 +26,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        retturn redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login'))
         title = "New Account"
-        
+
     return render_template('auth/register.html', registration_form = form)
