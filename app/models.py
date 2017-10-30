f64b45f81fe16df1dff9d7565e425f6dedb2e2eb
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-30 21:56:23 +0300

    add all classes

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    pitches =  db.relationship('Pitch', backref = 'user', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), index = True)
+    pitches = db.relationship('Pitch', backref = 'category', lazy = "dynamic")
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255), index = True)
+    post = db.Column(db.String(300), index = True)
+    vote = db.Column(db.Integer)
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    reviews = db.relationship('Review', backref = 'pitch', lazy = "dynamic")
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_review = db.Column(db.String(255), index = True)
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
