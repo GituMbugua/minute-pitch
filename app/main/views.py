a0be6b6099f7cfc60e107f0838c30e80d9324c08
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-31 15:19:12 +0300

    add route to categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,22 @@
 from flask import render_template, redirect, url_for
 from . import main
-from ..models import Review
+from ..models import Category, Review
 
 @main.route('/')
 def index():
     '''
     view root page function that returns index page
     '''
-    return render_template('index.html')
+    category = Category.get_categories()
+
+    title = 'Home'
+    return render_template('index.html', title = title, category = category)
+
+@main.route('/category/<int:id>')
+def category(id):
+    '''
+    view category function that returns the pitches of that category
+    '''
+    category = Category.query.get(id)
+
+    return render_template('category.html', category = category)
