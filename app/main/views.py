85d7893a5a60941a626e5692e27790ab1f5aa1c4
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-31 20:26:41 +0300

    add route for new pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for
 from . import main
-from ..models import Category, Review
+from .forms import PitchForm
+from ..models import Category, Pitch, Review
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -18,5 +20,25 @@ def category(id):
     view category function that returns the pitches of that category
     '''
     category = Category.query.get(id)
+    title = f'{category.name} pitches'
+    pitch = Pitch.get_pitches(category.id)
 
-    return render_template('category.html', category = category)
+    return render_template('category.html', title = title, category = category, pitch = pitch)
+
+@main.route('/category/pitch/new/<int:id>', methods = ["GET", "POST"])
+@login_required
+def new_pitch(id):
+    form = PitchForm()
+    category = Category.query.filter_by(id = id).first()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+
+        # pitch instance
+        new_pitch = Pitch(category_id = category.id, title = title, post = post)
+
+        # save pitch
+        new_pitch.save_pitch()  
+        return redirect(url_for('.category', id = category.id))
+    title = f'{category.name} pitches'
+    return render_template('new_pitch.html', title = title, pitch_form = form, category = category)
